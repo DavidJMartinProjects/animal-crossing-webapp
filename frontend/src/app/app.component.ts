e5734c1457cd342e6aea322fb59fb8ab8d033c16
unknown <davidjmartin@hotmail.com>
2021-06-27 21:35:35 +0100

    checkin

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -34,144 +34,51 @@ export class AppComponent {
           lengthMenu: [5, 10, 25]
         });
       }, 1);
-    }, error => console.error(error));    
+    }, error => console.error(error));
   }
 
   showEditUserForm(game: Game) {
     console.log("edit game: " + game.description);
     this.currentGame = game;
     console.log("currentGame set.");
-    $('#exampleModal').modal('show');    
+    $('#exampleModal').modal('show');
     $('#startDateInput').val(game.startDate);
     $('#startTimeInput').val(game.startTimes[0].startTime);
     $('#descriptionInput').val(game.description);
-    
-  if (!game) {
-    this.userForm = false;
-    return;
+
+    if (!game) {
+      this.userForm = false;
+      return;
+    }
+    this.editUserForm = true;
+    this.editedUser = game;
   }
-  this.editUserForm = true;
-  this.editedUser = game;
-}
 
-removeUser(game: Game) {
-  // this.userService.deleteUser(user);
-  console.log("delete game: " + game.description);
-}
-
-showNewGnomeCrossingForm(){
-  console.log("new gnome crossing.")
-}
-
-addGnome(game: Game) {
-  console.log("new gnome crossing.")
-  
-$('body').removeClass('modal-open'); // For scroll run
-// $('#newGameModal').modal('hide');
-  // $('#newGameModal').modal('hide');    
-  // $('#newGnomeModal').modal('show');    
-}
-
-
-
-
-}
-
-// $("a.close").on("click", function(e){
-//   $("#modal").modal("hide");
-//   e.stopPropagation();
-// });
-
-$(document).on('click', '.panel-heading span.clickable', function(e) {
-  var $this = $(this);
-  if (!$this.hasClass('panel-collapsed')) {
-    $this.closest('.panel').find('.panel-body').slideUp();
-    $this.addClass('panel-collapsed');
-    $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
-  } else {
-    $this.closest('.panel').find('.panel-body').slideDown();
-    $this.removeClass('panel-collapsed');
-    $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+  removeUser(game: Game) {
+    // this.userService.deleteUser(user);
+    console.log("delete game: " + game.description);
   }
-});
-$(document).on('click', '.panel div.clickable', function(e) {
-  var $this = $(this);
-  if (!$this.hasClass('panel-collapsed')) {
-    $this.closest('.panel').find('.panel-body').slideUp();
-    $this.addClass('panel-collapsed');
-    $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
-  } else {
-    $this.closest('.panel').find('.panel-body').slideDown();
-    $this.removeClass('panel-collapsed');
-    $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
+
+  showNewGnomeCrossingForm() {
+    console.log("new gnome crossing.")
   }
+
+  addGnome(game: Game) {
+    console.log("new gnome crossing.")
+
+    $('body').removeClass('modal-open'); // For scroll run
+    // $('#newGameModal').modal('hide');
+    // $('#newGameModal').modal('hide');    
+    // $('#newGnomeModal').modal('show');  
+  }
+
+}
+
+$(document).ready(function () {
+
+  $('#group input:radio').on('change', function () {
+    var value = $(this).val();
+    console.log(value);
+  });
+
 });
-$(document).ready(function() {
-  $('.panel-heading span.clickable').click();
-  $('.panel div.clickable').click();
-});
-
-
-// users: User[];
-// userForm: boolean;
-// isNewUser: boolean;
-// newUser: any = {};
-// editUserForm: boolean;
-// editedUser: any = {};
-
-// constructor(private userService: UserService) { }
-
-// ngOnInit() {
-//   this.users = this.getUsers();
-// }
-
-// getUsers(): User[] {
-//   return this.userService.getUsersFromData();
-// }
-
-// showEditUserForm(user: User) {
-//   if (!user) {
-//     this.userForm = false;
-//     return;
-//   }
-//   this.editUserForm = true;
-//   this.editedUser = user;
-// }
-
-// showAddUserForm() {
-//   // resets form if edited user
-//   if (this.users.length) {
-//     this.newUser = {};
-//   }
-//   this.userForm = true;
-//   this.isNewUser = true;
-
-// }
-
-// saveUser(user: User) {
-//   if (this.isNewUser) {
-//     // add a new user
-//     this.userService.addUser(user);
-//   }
-//   this.userForm = false;
-// }
-
-// updateUser() {
-//   this.userService.updateUser(this.editedUser);
-//   this.editUserForm = false;
-//   this.editedUser = {};
-// }
-
-// removeUser(user: User) {
-//   this.userService.deleteUser(user);
-// }
-
-// cancelEdits() {
-//   this.editedUser = {};
-//   this.editUserForm = false;
-// }
-
-// cancelNewUser() {
-//   this.newUser = {};
-//   this.userForm = false;
-// }
