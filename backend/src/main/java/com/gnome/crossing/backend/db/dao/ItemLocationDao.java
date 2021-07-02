cbff6965467e9b9bcc4047fe2378113f4e21b19b
unknown <davidjmartin@hotmail.com>
2021-07-03 00:51:47 +0100

    refactorings

--- backend/src/main/java/com/gnome/crossing/backend/db/dao/ItemLocationDao.java
+++ backend/src/main/java/com/gnome/crossing/backend/db/dao/ItemLocationDao.java
@@ -37,41 +37,16 @@ public class ItemLocationDao implements DbOperation<ItemLocation> {
             .collect(Collectors.toList());
     }
 
-//    @Override
-//    public Customer findById(long customerId) {
-//        log.info("fetching itemLocation with id: {}.", customerId);
-//        CustomerEntity entity = customerRepository.findById(customerId)
-//            .orElseThrow(() -> new CustomerServiceException(String.format(CUSTOMER_ID_DOES_NOT_EXIST, customerId)));
-//        return mapper.toDto(entity);
-//    }
-//
     @Override
     public ItemLocation save(ItemLocation itemLocation) {
         log.info("saving itemLocation with desc: {}.", itemLocation.getDescription());
         return mapper.toDto(itemLocationRespository.save(mapper.toEntity(itemLocation)));
     }
-//
-//    @Override
-//    public Customer update(Customer customer) {
-//        log.info("updating customer with id: {}.", customer.getId());
-//        return mapper.toDto(customerRepository.save(mapper.toEntity(customer)));
-//    }
-//
+
     @Override
     public void deleteById(long customerId) {
         log.info("deleting customer with id: {}.", customerId);
         itemLocationRespository.deleteById(customerId);
     }
-//
-//    @Override
-//    public void deleteAll() {
-//        log.info("deleting all customers records.");
-//        customerRepository.deleteAll();
-//    }
-//
-//    @Override
-//    public boolean isEmailRegistered(String email) {
-//        return customerRepository.existsByEmail(email);
-//    }
 
 }
